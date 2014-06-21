0d7b07df8e031e9b14b5be8e0baaac6e3ba3e281
nielsx25 <naverduin@hotmail.com>
2014-06-21 22:59:48 +0200

    Episode 2 + 3 done.

---
+++ src/main/java/com/nielsx25/letsmod/LetsMod.java
@@ -0,0 +1,24 @@
+package com.nielsx25.letsmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.*;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+@Mod(modid = "letsmod", name = "Let's mod reboot", version = "1.7.2-0.1")
+public class LetsMod {
+	@Mod.Instance("LetsMod")
+	public static LetsMod instance;
+	
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event){
+		
+	}
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event){
+		
+	}
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event){
+		
+	}
+}
